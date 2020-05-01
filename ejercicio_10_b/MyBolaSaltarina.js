5fa61998881f8328427646ad9d5d84e33b0328c5
Galactic-O <pabloshiny@gmail.com>
2020-05-01 19:37:23 +0200

    Terminado ejercicio 10b

--- ejercicio_10_b/MyBolaSaltarina.js
+++ ejercicio_10_b/MyBolaSaltarina.js
@@ -1,41 +1,98 @@
-
 class MyBolaSaltarina extends THREE.Object3D {
-  constructor() {
+  constructor(gui,titleGui) {
     super();
 
-    var origen ={x:0, y: -4};
-    var destino ={x:0, y: 4};
-    var textura = new THREE.MeshNormalMaterial();
-    var texturaCil = new THREE.MeshNormalMaterial({opacity:0.35,transparent:true});
-    var geoBola = new THREE.SphereGeometry( 1, 20, 20 );
-    var geoCilindro = new THREE.CylinderGeometry (1,1,10,20);
-    geoBola.translate(11,0,0)
+    // Se crea la parte de la interfaz que corresponde a la caja
+    // Se crea primero porque otros métodos usan las variables que se definen para la interfaz
+    this.createGUI(gui,titleGui);
 
-    this.bola = new THREE.Mesh (geoBola, textura);
+    this.radio = 1.0;
+    this.ascendiendo = true;
+
+    var texturaBola = new THREE.MeshNormalMaterial();
+    var geoBola = new THREE.SphereGeometry( 1, 20, 20 );
+    this.bola = new THREE.Mesh (geoBola, texturaBola);
+    this.bola.position.x = 4;
+
+    var texturaCil = new THREE.MeshNormalMaterial({opacity:0.35,transparent:true});
+    var geoCilindro = new THREE.CylinderGeometry (1,1,10,20);
     this.cilindro = new THREE.Mesh (geoCilindro, texturaCil);
 
-    this.add (this.bola);
-    this.add (this.cilindro);
+    //this.bola.position.x = radio;
+    //this.bola.position.z = 1;
 
-    var movimiento = new TWEEN.Tween(origen).to(destino,700).repeat(Infinity).yoyo(true);
-    var that = this;
-    movimiento.onUpdate(function(){
-      that.bola.position.x = origen.x;
-      that.bola.position.y = origen.y;
-    });
+    // Creamos el siguiente nodo, que será el que rotaremos
+    this.bolaPosicionada = new THREE.Object3D ();
+    this.bolaPosicionada.add ( this.bola );
 
-    movimiento.start();
 
+    // Unimos las partes del Reloj
+    this.add(this.cilindro);
+    this.add(this.bolaPosicionada);
+
+    this.tiempoAnterior = Date.now();
   }
 
+  createGUI (gui,titleGui) {
+    /*
+    // Controles para el tamaño, la orientación y la posición de la caja
+    this.guiControls = new function () {
+      this.velocidad = 1.0;
+
+      // Un botón para dejarlo todo en su posición inicial
+      // Cuando se pulse se ejecutará esta función.
+      this.reset = function () {
+        this.velocidad = 1.0;
+      }
+    }
+
+    // Se crea una sección para los controles de la caja
+    var folder = gui.addFolder (titleGui);
+    // Estas lineas son las que añaden los componentes de la interfaz
+    // Las tres cifras indican un valor mínimo, un máximo y el incremento
+    // El método   listen()   permite que si se cambia el valor de la variable en código, el deslizador de la interfaz se actualice
+    folder.add (this.guiControls, 'velocidad', -5.0, 5.0, 0.1).name ('Velocidad: ').listen();
+    folder.add (this.guiControls, 'reset').name ('[ Reset ]');
+    */
+  }
+
+   updateGeometry() {
+     this.sphere.geometry = new THREE.SphereGeometry (this.guiControls.radius, this.guiControls.segments, this.guiControls.segments);
+   }
 
   update (radio) {
+    // Con independencia de cómo se escriban las 3 siguientes líneas, el orden en el que se aplican las transformaciones es:
+    // Primero, el escalado
+    // Segundo, la rotación en Z
+    // Después, la rotación en Y
+    // Luego, la rotación en X
+    // Y por último la traslación
+    var tiempoActual = Date.now();
+    var segundosTranscurridos = (tiempoActual - this.tiempoAnterior) / 1000;
+
     this.cilindro.scale.set(radio,1,radio);
-    this.bola.rotation.y += 0.05;
-    this.bola.position.x = radio;
+    this.bolaPosicionada.rotation.y -= 1.5 * segundosTranscurridos;
+    this.bola.position.x = radio+1;
 
+    var posicionVertical = this.bola.position.y;
+    if(this.ascendiendo) {
+      if ( (posicionVertical+0.1) > 4.0   ) {
+         this.ascendiendo=false;
+         this.bola.position.y -= 0.1;
+      } else {
+         this.bola.position.y += 0.1;
+      }
+   } else {
+      if ( (this.bola.position.y - 0.1) < -4 ) {
+         this.ascendiendo = true;
+         this.bola.position.y += 0.1;
+      } else {
+         this.bola.position.y -= 0.1;
+      }
 
-    TWEEN.update();
+    }
 
-}
+    this.tiempoAnterior = tiempoActual;
+
+  }
 }
--- ejercicio_10_b/MyScene.js
+++ ejercicio_10_b/MyScene.js
@@ -34,7 +34,7 @@ class MyScene extends THREE.Scene {
     // Por último creamos el modelo.
     // El modelo puede incluir su parte de la interfaz gráfica de usuario. Le pasamos la referencia a
     // la gui y el texto bajo el que se agruparán los controles de la interfaz que añada el modelo.
-    this.bola = new MyBolaSaltarina();
+    this.bola = new MyBolaSaltarina2(this.gui, "illonose");
 
     this.add(this.bola);
 
