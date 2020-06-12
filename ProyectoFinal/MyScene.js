07bb2307782b10dac40c1923dad28ab1c5136909
Galactic-O <pabloshiny@gmail.com>
2020-06-12 18:52:58 +0200

    Modo Retro activable/desactivable

--- ProyectoFinal/MyScene.js
+++ ProyectoFinal/MyScene.js
@@ -592,11 +592,19 @@ class MyScene extends THREE.Scene {
       case 39 : // Cursor a la derecha
         this.motoJugador.right = true;
         break;
+      case 82 : // Activar/desactivar modo retro
+        if (!this.postprocessingActivado) {
+           this.postprocessingActivado = true;
+        } else {
+           this.postprocessingActivado = false;
+        }
+        break;
     }
   }
 
   onKeyUp (event) {
     var key = event.which || event.keyCode;
+    console.log(key);
     switch (key) {
       case 37 : // Cursor a la izquierda
         this.motoJugador.left = false;
@@ -632,8 +640,13 @@ class MyScene extends THREE.Scene {
     //this.spotLights.intensity = this.guiControls.lightIntensity;
 
     // Le decimos al renderizador "visualiza la escena que te indico usando la cámara que te estoy pasando"
-    //this.renderer.render (this, this.getCamera());
-    this.composer.render();
+    //
+
+    if ( this.postprocessingActivado ) {
+		this.composer.render();
+	 } else {
+		this.renderer.render (this, this.getCamera());
+	 }
 
 
     switch (this.gameState) {
@@ -910,96 +923,6 @@ MyScene.nivelActual = 0;
 function setNivel(nivel){
  MyScene.nivelActual = nivel;
 }
-
-////////////////////////
-///  SCREEN SHAKE
-////////////////////////
-
-function ScreenShake() {
-
- return {
-
-    // When a function outside ScreenShake handle the camera, it should
-    // always check that ScreenShake.enabled is false before.
-    enabled: false,
-
-    _timestampStart: undefined,
-
-    _timestampEnd: undefined,
-
-    _startPoint: undefined,
-
-    _endPoint: undefined,
-
-
-    // update(camera) must be called in the loop function of the renderer,
-    // it will repositioned the camera according to the requested shaking.
-    update: function update(camera) {
-      if ( this.enabled == true ) {
-        const now = Date.now();
-        if ( this._timestampEnd > now ) {
-          let interval = (Date.now() - this._timestampStart) /
-            (this._timestampEnd - this._timestampStart) ;
-          this.computePosition( camera, interval );
-        } else {
-          camera.position.copy(this._startPoint);
-          this.enabled = false;
-        };
-      };
-    },
-
-
-    // This initialize the values of the shaking.
-    // vecToAdd param is the offset of the camera position at the climax of its wave.
-    shake: function shake(camera, vecToAdd, milliseconds) {
-      this.enabled = true ;
-      this._timestampStart = Date.now();
-      this._timestampEnd = this._timestampStart + milliseconds;
-      this._startPoint = new THREE.Vector3().copy(camera.position);
-      this._endPoint = new THREE.Vector3().addVectors( camera.position, vecToAdd );
-    },
-
-
-    computePosition: function computePosition(camera, interval) {
-
-      // This creates the wavy movement of the camera along the interval.
-      // The first bloc call this.getQuadra() with a positive indice between
-      // 0 and 1, then the second call it again with a negative indice between
-      // 0 and -1, etc. Variable position will get the sign of the indice, and
-      // get wavy.
-      if (interval < 0.4) {
-        var position = this.getQuadra( interval / 0.4 );
-      } else if (interval < 0.7) {
-        var position = this.getQuadra( (interval-0.4) / 0.3 ) * -0.6;
-      } else if (interval < 0.9) {
-        var position = this.getQuadra( (interval-0.7) / 0.2 ) * 0.3;
-      } else {
-        var position = this.getQuadra( (interval-0.9) / 0.1 ) * -0.1;
-      }
-
-      // Here the camera is positioned according to the wavy 'position' variable.
-      camera.position.lerpVectors( this._startPoint, this._endPoint, position );
-    },
-
-    // This is a quadratic function that return 0 at first, then return 0.5 when t=0.5,
-    // then return 0 when t=1 ;
-    getQuadra: function getQuadra(t) {
-      return 9.436896e-16 + (4*t) - (4*(t*t)) ;
-    }
-
- };
-
-};
-
-
-//////////////////////
-////  EVENT
-//////////////////////
-
-function shake() {
-   screenShake.shake( camera, new THREE.Vector3(0.5, -1, 1.5), 250 );
-}
-
 
 /// La función   main
 $(function () {
